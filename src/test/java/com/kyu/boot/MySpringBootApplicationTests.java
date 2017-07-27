a014f481f416dcbfd0ab8335459b8456c803a230
이남규 <nklee@ncsoft.com>
2017-07-27 17:05:04 +0900

    @ComponentScan 디폴트 전략 확인 테스트 - @SpringBootTest 가 선언된 클래스의 패키지부터 그 하위까지 스캔한다.

--- src/test/java/com/kyu/boot/MySpringBootApplicationTests.java
+++ src/test/java/com/kyu/boot/MySpringBootApplicationTests.java
@@ -1,13 +1,27 @@
 package com.kyu.boot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MySpringBootApplicationTests {
+
+    @Autowired
+    private String testBean;
+
+    @Test
+    public void 빈스캔테스트() {
+        assertThat("testBean", is(testBean));
+
+    }
 
 }
